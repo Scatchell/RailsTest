da9bde7e24c7f747cfaf5ed55afc0797b147d87c
Anthony <Scatchell@gmail.com>
2012-02-16 14:53:53 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RampUpSpike1::Application.config.secret_token = '92b1a718947022cc7a29ed43799c09fbc7861ec7418fa02688d3326597b9ffc0a9eafa2f73dc1e568339ad133f6633cb3014bb22c5b0bded6c6072a1138f766e'
